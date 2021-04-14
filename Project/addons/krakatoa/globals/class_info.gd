cf077b6c9eb269b885f317491c1648414927e893
Rabbit <rabbit@dfaction.net>
2021-04-13 22:14:03 -0700

    Refactor ClassInfo to simplify implementation
    Changed ClassInfo to use a non-static class in the background, to greatly simplify the code implementation. The outward facing API of ClassInfo is still static.

--- Project/addons/krakatoa/globals/class_info.gd
+++ Project/addons/krakatoa/globals/class_info.gd
@@ -30,115 +30,149 @@ static func get_built_in_class(type) -> String:
 
 
 static func class_inherits_from(a_class, ancestor) -> bool:
-	if a_class is Script:
-		return script_inherits_from(a_class, ancestor)
-	elif typeof(a_class) == TYPE_STRING:
-		if is_built_in_class(a_class):
-			return is_built_in_class(ancestor) && \
-					ClassDB.is_parent_class(a_class, get_built_in_class(ancestor))
-		else:
-			var class_script: Script = load(a_class)
-			if class_script == null:
-				assert(false, "%s is an invalid script path" % a_class)
-				return false
-
-			return script_inherits_from(class_script, ancestor)
-	elif typeof(a_class) == TYPE_OBJECT:
-		var class_script: Script = a_class.get_script()
-		if class_script == null:
-			return is_built_in_class(ancestor) && \
-					ClassDB.is_parent_class(a_class, get_built_in_class(ancestor))
-
-		return script_inherits_from(class_script, ancestor)
-
-	return false
-
-
-static func script_inherits_from(a_class: Script, ancestor) -> bool:
-	if ancestor is Script:
-		return script_inherits_from_script(a_class, ancestor as Script)
-	elif typeof(ancestor) == TYPE_STRING:
-		if ClassDB.class_exists(ancestor):
-			return ClassDB.is_parent_class(a_class.get_instance_base_type(), ancestor)
-		else:
-			var ancestor_script: Script = load(ancestor)
-			if ancestor_script == null:
-				assert(false, "%s is an invalid script path" % ancestor)
-				return false
-
-			return script_inherits_from_script(a_class, ancestor_script)
-	elif typeof(ancestor) == TYPE_OBJECT:
-		var ancestor_script: Script = ancestor.get_script()
-		if ancestor_script == null:
-			return ClassDB.is_parent_class(a_class.get_instance_base_type(), ancestor.get_class())
-		else:
-			return script_inherits_from_script(a_class, ancestor_script)
-
-	return false
-
-
-static func script_inherits_from_script(a_script: Script, ancestor: Script) -> bool:
-	var base_script: Script = a_script.get_base_script()
-	while base_script != null:
-		if ancestor == base_script:
-			return true
-		base_script = base_script.get_base_script()
-
-	return false
+	var class_proxy: ClassInfoProxy = ClassInfoProxy.new(a_class)
+	var ancestor_proxy: ClassInfoProxy = ClassInfoProxy.new(ancestor)
+	return class_proxy.inherits_from(ancestor_proxy)
 
 
 static func class_has_signal(for_class, signal_name: String) -> bool:
-	if for_class is Script:
-		return script_has_signal(for_class, signal_name)
-	elif typeof(for_class) == TYPE_STRING:
-		if ClassDB.class_exists(for_class):
-			return ClassDB.class_has_signal(for_class, signal_name)
-		else:
-			var class_script: Script = load(for_class)
-			if class_script == null:
-				assert(false, "%s is an invalid script path" % for_class)
-				return false
-
-			return script_has_signal(class_script, signal_name)
-	elif typeof(for_class) == TYPE_OBJECT:
-		return for_class.has_signal(signal_name)
-
-	return false
-
-
-static func script_has_signal(for_script: Script, signal_name: String) -> bool:
-	if for_script.has_script_signal(signal_name):
-		return true
-
-	return ClassDB.class_has_signal(for_script.get_instance_base_type(), signal_name)
+	var proxy: ClassInfoProxy = ClassInfoProxy.new(for_class)
+	return proxy.has_signal(signal_name)
 
 
 static func class_get_signal(from_class, signal_name: String) -> Dictionary:
-	if from_class is Script:
-		return script_get_signal(from_class, signal_name)
-	elif typeof(from_class) == TYPE_STRING:
-		if ClassDB.class_exists(from_class):
-			return ClassDB.class_get_signal(from_class, signal_name)
-		else:
-			var class_script: Script = load(from_class)
-			if class_script == null:
-				assert(false, "%s is an invalid script path" % from_class)
+	var proxy: ClassInfoProxy = ClassInfoProxy.new(from_class)
+	return proxy.get_signal(signal_name)
+
+
+class ClassInfoProxy extends Reference:
+
+
+	enum {
+		UNKNOWN,
+		BUILT_IN,
+		SCRIPT_DB,
+		LOOSE_SCRIPT
+	}
+
+	var _class: String
+	var _type: int
+	var _script: Script
+
+
+	func _init(from_class) -> void:
+		match typeof(from_class):
+			TYPE_STRING:
+				if ScriptClassDB.class_exists(from_class):
+					_class = ScriptClassDB.class_get_name(from_class)
+					_script = ScriptClassDB.class_get_script(from_class)
+					_type = SCRIPT_DB
+				elif ClassDB.class_exists(from_class):
+					_class = from_class
+					_type = BUILT_IN
+				else:
+					_class = from_class
+					_script = load(from_class)
+					_type = UNKNOWN if _script == null else LOOSE_SCRIPT
+			TYPE_OBJECT:
+				var obj_script: Script = from_class.get_script() if \
+						!(from_class is Script) else from_class
+
+				if obj_script == null:
+					_class = from_class.get_class()
+					_type = BUILT_IN
+				elif ScriptClassDB.class_exists(obj_script.resource_path):
+					_class = ScriptClassDB.class_get_name(obj_script.resource_path)
+					_script = obj_script
+					_type = SCRIPT_DB
+				else:
+					_class = obj_script.resource_name
+					_script = obj_script
+					_type = LOOSE_SCRIPT
+
+
+	func get_type() -> int:
+		return _type
+
+
+	func get_class() -> String:
+		return _class
+
+
+	func get_script() -> Script:
+		return _script
+
+
+	func inherits_from(ancestor: ClassInfoProxy) -> bool:
+		match _type:
+			BUILT_IN:
+				return ancestor.get_type() == BUILT_IN && \
+						ClassDB.is_parent_class(_class, ancestor.get_class())
+			SCRIPT_DB:
+				match ancestor.get_type():
+					BUILT_IN:
+						return ClassDB.is_parent_class(
+								_script.get_instance_base_type(),
+								ancestor.get_class())
+					SCRIPT_DB:
+						return ScriptClassDB.is_parent_class(
+								_class,
+								ancestor.get_class())
+					LOOSE_SCRIPT:
+						var base_script: Script = _script
+						while base_script != null:
+							if base_script == ancestor.get_script():
+								return true
+							base_script = base_script.get_base_script()
+
+						return false
+					_:
+						return false
+			LOOSE_SCRIPT:
+				match ancestor.get_type():
+					BUILT_IN:
+						return ClassDB.is_parent_class(
+								_script.get_instance_base_type(),
+								ancestor.get_class())
+					SCRIPT_DB, LOOSE_SCRIPT:
+						var base_script: Script = _script
+						while base_script != null:
+							if base_script == ancestor.get_script():
+								return true
+							base_script = base_script.get_base_script()
+
+						return false
+					_:
+						return false
+			_:
+				return false
+
+
+	func has_signal(signal_name: String) -> bool:
+		match _type:
+			BUILT_IN:
+				return ClassDB.class_has_signal(_class, signal_name)
+			SCRIPT_DB:
+				return ScriptClassDB.class_has_signal(_class, signal_name)
+			LOOSE_SCRIPT:
+				if _script.has_script_signal(signal_name):
+					return true
+				return ClassDB.class_has_signal(_script.get_instance_base_type(), signal_name)
+			_:
+				return false
+
+
+	func get_signal(signal_name: String) -> Dictionary:
+		match _type:
+			BUILT_IN:
+				return ClassDB.class_get_signal(_class, signal_name)
+			SCRIPT_DB:
+				return ScriptClassDB.class_get_signal(_class, signal_name)
+			LOOSE_SCRIPT:
+				for signal_info in _script.get_signal_list():
+					if signal_info.name == signal_name:
+						return signal_info
 				return {}
-
-			return script_get_signal(class_script, signal_name)
-	elif typeof(from_class) == TYPE_OBJECT:
-		for signal_info in from_class.get_signal_list():
-			if signal_info.name == signal_name:
-				return signal_info
-
-	return {}
-
-
-static func script_get_signal(from_script: Script, signal_name: String) -> Dictionary:
-	if from_script.has_script_signal(signal_name):
-		for signal_info in from_script.get_script_signal_list():
-			if signal_info.name == signal_name:
-				return signal_info
-
-	return ClassDB.class_get_signal(from_script.get_instance_base_type(), signal_name)
+			_:
+				return {}
 
