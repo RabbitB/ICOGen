5d05e0086eb97b49522706b8ad733c04df357a29
Rabbit <rabbit@dfaction.net>
2021-04-19 15:09:14 -0700

    Update the preview portion of ImageEntry
    Part of the update removes the need for all the previously generated placeholder and indicator images, so remove all of them as well.

--- Project/ui_elements/image_entry/preview/preview.gd
+++ Project/ui_elements/image_entry/preview/preview.gd
@@ -4,14 +4,41 @@ extends Control
 
 export(ICOGenData.ImageSize) var image_size: int = ICOGenData.ImageSize.x16 setget _set_image_size
 
+var _placeholder_texture
+
 onready var _viewer: TextureRect = $Viewer as TextureRect
 
 
-func _set_image_size(new_size: int) -> void:
-	var active_data: ICOGenData = ICOGen.get_active_data()
-	image_size = new_size
+func _ready() -> void:
+	_placeholder_texture = _viewer.texture
 
-	var texture: ImageTexture = ImageTexture.new()
-	texture.create_from_image(active_data.get_image(image_size))
-	_viewer.texture = texture
+# warning-ignore:return_value_discarded
+	ICOGen.get_signal_relay().connect_relay(
+			"image_refreshed",
+			ICOGen.get_active_data(),
+			self,
+			"_on_image_refreshed")
+
+
+func clear_image() -> void:
+	_viewer.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+	_viewer.texture = _placeholder_texture
+
+
+func _set_image_size(new_size: int) -> void:
+	image_size = new_size
+	_on_image_refreshed(image_size, self)
+
+
+func _on_image_refreshed(for_size: int, _source: Object) -> void:
+	if Engine.editor_hint:
+		return
+
+	if image_size == for_size:
+		var active_data: ICOGenData = ICOGen.get_active_data()
+		var texture: ImageTexture = ImageTexture.new()
+
+		texture.create_from_image(active_data.get_image(image_size))
+		_viewer.texture = texture
+		_viewer.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
 
